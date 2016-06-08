66a852e0c5501f40cbb46802a692843b3aa97341
polaris <polaris@studygolang.com>
2016-06-08 22:58:42 +0800

    删除老版本

--- websites/code/studygolang/src/filter/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2013 The StudyGolang Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-// http://studygolang.com
-// Author：polaris	studygolang@gmail.com
-
-// 实现 https://github.com/studygolang/mux 中的Filter接口
-package filter
