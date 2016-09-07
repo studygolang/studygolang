32141279e63198c1a1c2a3fbc086d5fac85adb0c
polaris <polaris@studygolang.com>
2016-09-07 14:43:36 +0800

    项目管理后台

--- src/http/controller/admin/routes.go
+++ src/http/controller/admin/routes.go
@@ -11,6 +11,7 @@ import "github.com/labstack/echo"
 func RegisterRoutes(g *echo.Group) {
 	new(AuthorityController).RegisterRoute(g)
 	new(ArticleController).RegisterRoute(g)
+	new(ProjectController).RegisterRoute(g)
 	new(RuleController).RegisterRoute(g)
 	new(ReadingController).RegisterRoute(g)
 	new(ToolController).RegisterRoute(g)
