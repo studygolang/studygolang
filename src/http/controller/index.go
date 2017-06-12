319b0f94071489bbd2e6280c6cf558d792090ee8
xuxinhua <xuxinhua@zhimadj.com>
2017-06-12 12:29:38 +0800

    节点导航

--- src/http/controller/index.go
+++ src/http/controller/index.go
@@ -44,6 +44,7 @@ func (IndexController) NewIndex(ctx echo.Context) error {
 	SetCookie(ctx, "INDEX_TAB", tab)
 
 	data := logic.DefaultIndex.FindData(ctx, tab)
+	data["nodes"] = logic.GenNodes()
 
 	return render(ctx, "new_index.html", data)
 }
