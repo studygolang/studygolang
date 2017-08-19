51f53b7aeb74a83f12d8a68a691f7591255d6fad
xuxinhua <xuxinhua@zhimadj.com>
2017-08-19 17:21:24 +0800

    hot node

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -274,7 +274,7 @@ $(function(){
 
 			var content = '';
 			for(var i in data) {
-				content += '<li><a href="/topics/node/'+data[i].nid+'?fr=sidebar" title="'+data[i].name+'">'+data[i].name+'</a></li>';
+				content += '<li><a href="/go/'+data[i].ename+'?fr=sidebar" title="'+data[i].name+'">'+data[i].name+'</a></li>';
 			}
 			
 			$('.sb-content .node-list ul').html(content);
