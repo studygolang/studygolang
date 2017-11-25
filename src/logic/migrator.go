8902d66835c3eca5fad136685938fbb4618d8e6d
ganglin.song <ganglin.song@yourdream.cc>
2017-11-26 01:50:27 +0800

    feat: add file header

--- src/logic/migrator.go
+++ src/logic/migrator.go
@@ -1,3 +1,9 @@
+// Copyright 2017 The StudyGolang Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// http://studygolang.com
+// Author:javasgl	songganglin@gmail.com
+
 package logic
 
 import (
