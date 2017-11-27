4410f4a6e2a7bed7121c0f7201530ea14cd9c866
ganglin.song <ganglin.song@yourdream.cc>
2017-11-27 17:15:04 +0800

    feat: 个人主页查看更多评论

--- src/logic/comment.go
+++ src/logic/comment.go
@@ -330,3 +330,64 @@ type CommentObjecter interface {
 	// commentMap 中的 key 是属主 id
 	SetObjinfo(ids []int, commentMap map[int][]*model.Comment)
 }
+
+// FindAll 支持多页翻看
+func (self CommentLogic) FindAll(ctx context.Context, paginator *Paginator, orderBy string, querystring string, args ...interface{}) []*model.Comment {
+	objLog := GetLogger(ctx)
+
+	comments := make([]*model.Comment, 0)
+	session := MasterDB.OrderBy(orderBy)
+	if querystring != "" {
+		session.Where(querystring, args...)
+	}
+	err := session.Limit(paginator.PerPage(), paginator.Offset()).Find(&comments)
+	if err != nil {
+		objLog.Errorln("CommentLogical FindAll error:", err)
+		return nil
+	}
+
+	cmtMap := make(map[int][]*model.Comment, len(model.PathUrlMap))
+	for _, comment := range comments {
+		self.decodeCmtContent(ctx, comment)
+
+		if _, ok := cmtMap[comment.Objtype]; !ok {
+			cmtMap[comment.Objtype] = make([]*model.Comment, 0, 10)
+		}
+
+		cmtMap[comment.Objtype] = append(cmtMap[comment.Objtype], comment)
+	}
+
+	cmtObjs := []CommentObjecter{
+		model.TypeTopic:    TopicComment{},
+		model.TypeArticle:  ArticleComment{},
+		model.TypeResource: ResourceComment{},
+		model.TypeWiki:     nil,
+		model.TypeProject:  ProjectComment{},
+		model.TypeBook:     BookComment{},
+	}
+	for cmtType, cmts := range cmtMap {
+		self.fillObjinfos(cmts, cmtObjs[cmtType])
+	}
+	return comments
+}
+
+// Count 获取用户全部评论数
+func (CommentLogic) Count(ctx context.Context, querystring string, args ...interface{}) int64 {
+	objLog := GetLogger(ctx)
+
+	var (
+		total int64
+		err   error
+	)
+	if querystring == "" {
+		total, err = MasterDB.Count(new(model.Comment))
+	} else {
+		total, err = MasterDB.Where(querystring, args...).Count(new(model.Comment))
+	}
+
+	if err != nil {
+		objLog.Errorln("CommentLogic Count error:", err)
+	}
+
+	return total
+}
